4286ae57234cd3527789befb268abbe76c88a5e7
982418058 <982418058@qq.com>
2019-11-18 18:13:28 +0800

    0.0.2 login

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,6 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
     </div>
     <router-view/>
   </div>
@@ -18,7 +16,9 @@
 }
 
 #nav {
-  padding: 30px;
+    max-width: 1080px;
+    height: 75px;
+    margin: 0 auto;
 }
 
 #nav a {
---
+++ src/components/team.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="team">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'team',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+</style>
