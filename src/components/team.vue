dc7d36cc64b0fb94bc437599cc8604eacac1ede2
982418058 <982418058@qq.com>
2019-12-05 17:30:19 +0800

    Signed-off-by: 982418058 <982418058@qq.com>

--- src/components/team.vue
+++ src/components/team.vue
@@ -16,37 +16,35 @@
       </b-col>
     </b-row>
     <b-collapse id="collapse-2">
-      <b-card>基础、交互、连接
+      <b-card>
           <b-form-group id="input-group-1" label="详细信息" label-for="input-1" description="输入145字以内的文字信息。" >
-            <b-form-input id="input-1" v-model="form.ininfo" type="ininfo" placeholder="输入文字..."></b-form-input>
+            <b-form-input id="input-1" v-model="form.ininfo" placeholder="输入文字..."></b-form-input>
           </b-form-group>
           <b-form-group id="input-group-1" label="封面" label-for="input-1" description="输入封面地址" >
-            <b-form-input id="input-1" v-model="form.img" type="img" placeholder="输入文字..."></b-form-input>
+            <b-form-input id="input-1" v-model="form.img" placeholder="图片地址..."></b-form-input>
           </b-form-group>
-        <b-card class="mt-3" header="Form Data Result">
-          <pre class="m-0">{{ form }}</pre>
-        </b-card>
       </b-card>
     </b-collapse>
     <div  class="bv-example-row">
        <b-card-group  columns>
             <b-card
-            overlay
             v-bind:img-src="data.img"
-            style="max-width: 20rem;margin: 5px auto; min-height: 4rem;"
+            style="max-width: 20rem;margin: 5px auto; min-height: 5.6rem;"
             v-for="data in data"
             :key="data.id"
+            @click="onmd(data)"
             >
-            <b-card-text>{{data.text}}</b-card-text>
-            <b-dropdown text="" variant="link" class="m-md-2 team_drop" right>
-              <b-dropdown-header id="dropdown-header-1">编辑</b-dropdown-header>
-              <b-dropdown-item-button aria-describedby="dropdown-header-1">重命名</b-dropdown-item-button>
-              <b-dropdown-item-button aria-describedby="dropdown-header-1">删除</b-dropdown-item-button>
-              <b-dropdown-header id="dropdown-header-2">状态</b-dropdown-header>
-              <b-dropdown-item-button aria-describedby="dropdown-header-2">进度</b-dropdown-item-button>
-              <b-dropdown-divider></b-dropdown-divider>
-              <b-dropdown-item-button> 更多</b-dropdown-item-button>
-            </b-dropdown>
+              <b-card-header>{{data.text}}</b-card-header>
+              <b-card-text>{{data.content}}</b-card-text>
+              <b-dropdown variant="link" class="m-md-2 team_drop" right>
+                <b-dropdown-header id="dropdown-header-1">编辑</b-dropdown-header>
+                <b-dropdown-item-button aria-describedby="dropdown-header-1">重命名</b-dropdown-item-button>
+                <b-dropdown-item-button aria-describedby="dropdown-header-1">删除</b-dropdown-item-button>
+                <b-dropdown-header id="dropdown-header-2">状态</b-dropdown-header>
+                <b-dropdown-item-button aria-describedby="dropdown-header-2">进度</b-dropdown-item-button>
+                <b-dropdown-divider></b-dropdown-divider>
+                <b-dropdown-item-button> 更多</b-dropdown-item-button>
+              </b-dropdown>
             </b-card>
        </b-card-group>
     </div>
@@ -60,34 +58,40 @@ export default {
       addname: '',
       items: [
         {
-          text: '1'
+          text: '我的'
         }
       ],
       data: [
         {
-          img: '',
-          text: 'Y任务',
-          id: '4'
+          img: '', // 封面
+          text: 'Y任务', // 任务名称
+          id: '4',
+          content: 'BootstrapVue更改日志，设置和 其他各种资源', // 备注
+          CreateDate: ''// 开始时间
         },
         {
           img: 'https://picsum.photos/600/300/?image=23',
           text: 'X小组',
-          id: '1'
+          id: '1',
+          content: 'BootstrapVue更改日志，设置和其他各种资源'
         },
         {
           img: 'https://picsum.photos/600/300/?image=24',
           text: 'X任务',
-          id: '2'
+          id: '2',
+          content: 'BootstrapVue更改日志，设置和其他各种资源'
         },
         {
           img: 'https://picsum.photos/600/300/?image=25',
           text: 'Y任务',
-          id: '3'
+          id: '3',
+          content: 'BootstrapVue更改日志，设置和其他各种资源'
         },
         {
           img: '',
           text: 'Y任务',
-          id: '5'
+          id: '5',
+          content: 'BootstrapVue更改日志，设置和其他各种资源'
         }
       ],
       form: {
@@ -104,36 +108,23 @@ export default {
     })
   },
   methods: {
-    onSubmit (evt) {
-      evt.preventDefault()
-      alert(JSON.stringify(this.form))
-    },
-    onReset (evt) {
-      evt.preventDefault()
-      // Reset our form values
-      this.form.email = ''
-      this.form.name = ''
-      this.form.food = null
-      this.form.checked = []
-      // Trick to reset/clear native browser form validation state
-      this.show = false
-      this.$nextTick(() => {
-        this.show = true
-      })
-    },
     creat () {
       this.data.unshift({
         img: this.form.img,
         text: this.addname,
-        id: ''
+        id: '',
+        content: this.form.ininfo
+      })
+    },
+    onmd (e) {
+      this.items.push({
+        text: e.text
       })
     }
   }
 }
 
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style>
 .team{
   max-width: 1080px;
@@ -147,12 +138,30 @@ export default {
 .content{
   margin: 5px 0;
 }
-.card-img-overlay{
-  text-align: left
-}
 .team_drop{
-  top: 6px;
-  right: 0px;
+  top: 19px;
+  right: 16px;
   position: absolute !important;
+}
+.card-header {
+    background: none !important;
+}
+.card-body {
+    position: relative;
+}
+.card:hover{
+    border: 1px solid #c3c3c3;
+    cursor: pointer;
+}
+
+.card::after{
+  display:block;
+  content:attr(title);
+  font-weight:bold;
+  visibility:hidden;
+  height:1px;
+  color:transparent;
+  margin-bottom:-1px;
+  overflow:hidden;
 }
 </style>
